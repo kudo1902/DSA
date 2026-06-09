ead40bacb10244b17d093694a2de0242bbb3672d
Nguyen Truong <nhattruong19021997@gmail.com>
2026-06-09 20:47:21 +0700

    add sol

---
+++ algorithms/dynamic_programming/practice/leetcode_119_pascals_triangle_ii/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void solve(int numRows, vector<vector<int>> &res)
+{
+    vector<int> row{1};
+    res.push_back(row);
+    if (numRows == 1)
+    {
+        return;
+    }
+
+    for (int i = 1; i < numRows; i++)
+    {
+        vector<int> newRow{1};
+        for (int j = 1; j < res.back().size(); j++)
+        {
+            newRow.push_back(res.back()[j - 1] + res.back()[j]);
+        }
+        newRow.push_back(1);
+        res.push_back(newRow);
+    }
+}
+
+vector<int> getRow(int rowIndex)
+{
+    vector<vector<int>> res;
+    solve(rowIndex + 1, res);
+    return res.back();
+}
+
+void print(const vector<vector<int>> &res)
+{
+    for (auto row : res)
+    {
+        for (auto col : row)
+        {
+            cout << col << '\t';
+        }
+        cout << '\n';
+    }
+}
+
+int main()
+{
+    freopen("output.txt", "w", stdout);
+    return 0;
+}
